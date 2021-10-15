918256f2869cdf1a4c4c66a5c90852e2d8d35a5d
Alvaro <almunav@gmail.com>
2021-10-15 18:12:14 +0200

    Make screen clearable

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-const game_container = document.querySelector("#board");
+const eas_body = document.getElementById("eas-body");
 
-console.log(game_container);
+function createScreen(pixels) {
+    const screen = document.createElement("div");
+    screen.id = "screen"
+    screen.style.gridTemplateColumns = "repeat(" + pixels + ", 1fr)";
 
-for (let x = 0; x < 16; x++) {
-    for (let y = 0; y < 16; y++) {
-        const pixel = document.createElement("div");
-        pixel.classList.add("pixel");
-        pixel.setAttribute("id", "x" + x + "y" + y);
-        pixel.addEventListener("mouseover", () => {
-            pixel.classList.add("active")
-        })
-        game_container.appendChild(pixel);
+    for (let x = 0; x < pixels; x++) {
+        for (let y = 0; y < pixels; y++) {
+            const pixel = document.createElement("div");
+            pixel.classList.add("pixel");
+            pixel.setAttribute("id", "x" + x + "y" + y);
+            pixel.addEventListener("mouseover", () => {
+                pixel.classList.add("active");
+            })
+            screen.appendChild(pixel);
+        }
     }
+    eas_body.appendChild(screen);
+
+    console.log("pixels:" + pixels)
 }
+
+function clearScreen() {
+    document.getElementById("screen").remove()
+    let pixels = prompt("Enter a number of pixels per side (lower than 100):");
+    if (pixels > 100) {
+        pixels = 100;
+    }
+    createScreen(pixels);
+}
+
+createScreen(16);
